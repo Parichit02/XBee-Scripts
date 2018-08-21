48b45695acbde7cb71183fb53af58908e613eb6a
Parichit Kumar <parichitcdrr@gmail.com>
2018-08-21 18:18:59 -0400

    Marking XBee destination address

--- transmit_Xbee/transmit_Xbee.ino
+++ transmit_Xbee/transmit_Xbee.ino
@@ -9,7 +9,7 @@ void loop() {
 //  XBeeAddress64 addr64 = XBeeAddress64();
 //  addr64.setMsb(13A200);//XXXXX -> Msb address of router/end node
 //  addr64.setLsb(4106FEB4);//XXXXX -> Lsb address of router/end node 
-  XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4163D1D9);
+  XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4163D1D9); //SL and SH of the destination XBee
   ZBTxRequest zbTx = ZBTxRequest(addr64, data, sizeof(data));
   xbee.send(zbTx);
 
