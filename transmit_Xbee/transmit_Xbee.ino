3ae328cd8e917b6cab2542538a11255072396e35
Parichit Kumar <parichitcdrr@gmail.com>
2018-08-22 12:44:38 -0400

    Newest version

--- transmit_Xbee/transmit_Xbee.ino
+++ transmit_Xbee/transmit_Xbee.ino
@@ -1,20 +1,31 @@
 #include <XBee.h>
-XBee xbee = XBee();
-void setup() {
-  Serial1.begin(9600);
-  xbee.setSerial(Serial1);
-}
-void loop() {
-  uint8_t data[] = {'H','I'};
-//  XBeeAddress64 addr64 = XBeeAddress64();
-//  addr64.setMsb(13A200);//XXXXX -> Msb address of router/end node
-//  addr64.setLsb(4106FEB4);//XXXXX -> Lsb address of router/end node 
-  XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4163D1D9); //SL and SH of the destination XBee
-  ZBTxRequest zbTx = ZBTxRequest(addr64, data, sizeof(data));
-  xbee.send(zbTx);
 
-  digitalWrite(13, HIGH);
-  delay(100);
-  digitalWrite(13, LOW);
-  delay(100);
+// create the XBee object
+XBee xbee = XBee();
+
+uint8_t payload[2];
+// SH + SL Address of receiving XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x4106FEB4);
+
+
+int pin5 = 0;
+
+void setup() {
+Serial1.begin(9600);
+xbee.setSerial(Serial1);
+pinMode(13, OUTPUT);
+}
+
+void loop() {
+payload[0] = 0x00;
+payload[1] = 0x01;
+
+delay(3000);
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+xbee.send(zbTx);
+
+digitalWrite(13, HIGH);
+delay(1000);
+digitalWrite(13, LOW);
+delay(100);
 }
